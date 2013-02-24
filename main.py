e6e5601dedb8fd45ddde78ba08574878615cff64
Belly Ndiaye <bellyndiaye@gmail.com>
2013-02-24 20:19:06 +0000

    Adding the title
    adding a title and a frame container

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from theMenu import *
 if __name__ == '__main__':
     
     root= Tk()
+    root.title("Project Manager")
     Menus(root)
+    frame = Frame(width=768, height=576, bg="", colormap="new")
+    frame.pack()
     mainloop()    
